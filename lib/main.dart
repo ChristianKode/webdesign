7fe59f2ee33ac327e958378dce60d0fda4119a72
HeavyBenni <benjamin.csaplar005@gmail.com>
2023-01-25 13:01:36 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'UngAnsatt',
+      title: 'nsatt',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
