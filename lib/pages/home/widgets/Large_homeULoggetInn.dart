96b18265823a44e872534febc0adf17d55559ef8
Bolle Benni <Bruker@IKT-TippiToppi.iktService.local>
2023-02-07 10:07:47 +0100

    asdasd

--- lib/pages/home/widgets/Large_homeULoggetInn.dart
+++ lib/pages/home/widgets/Large_homeULoggetInn.dart
@@ -2,6 +2,7 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:webdesign/pages/home/widgets/adventues.dart';
 import 'package:webdesign/pages/home/widgets/home_info.dart';
 import 'package:webdesign/widgets/navbarLoggedIn.dart';
 import 'package:webdesign/widgets/navbarLoggedOut.dart';
@@ -11,15 +12,13 @@ class LargeHomeIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/images/home_background.jpg"),
-                fit: BoxFit.cover)),
-        child: Column(
-          children: <Widget>[NavBarIn(), HomeInfo()],
-        ),
-      ),
-    );
+        body: Column(
+                children: <Widget>[NavBarIn(), HomeInfo(),],
+              ),
+        
+          );
+        
+      
+    
   }
 }
--- lib/pages/newJob/widgets/body.dart
+++ lib/pages/newJob/widgets/body.dart
@@ -3,7 +3,6 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:image_picker_web/image_picker_web.dart';
 import 'package:image_picker/image_picker.dart';
-//import 'dart:io';
 
 
 
