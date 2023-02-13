f0ba055e45c110afbf87b943f12c1e381f36d44f
HeavyBenni <csaplar@elev.rogfk.no>
2023-02-13 23:12:54 +0100

    newjob update

--- lib/pages/home/widgets/job_list.dart
+++ lib/pages/home/widgets/job_list.dart
@@ -15,84 +15,63 @@ class JobList extends StatefulWidget {
 class _JobListState extends State<JobList> {
   final databaseRef = FirebaseDatabase.instance.ref().child("adventures");
   ScrollController _scrollController = ScrollController();
-  String downloadURL = '';
-
-    Future<void> downloadURLExample(String aid) async {
-    downloadURL = await FirebaseStorage.instance
-        .ref()
-        .child(aid)
-        .child('/')
-        .getDownloadURL();
-
-        
-  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      
-
-      
-      
-      child:
-       Row(
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
-        
         children: [
-          
           Container(
-            child: IconButton(icon: Icon(Icons.arrow_back), onPressed: (){
-                  _scrollController.animateTo(
+            child: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                _scrollController.animateTo(
                   _scrollController.offset - 150,
                   duration: Duration(milliseconds: 500),
                   curve: Curves.easeOut,
                 );
-            },),
-          ),
-            
-            Expanded(
-              child: Container(
-                height: 300,
-                child: FirebaseAnimatedList(
-                  physics: ScrollPhysics(parent: ClampingScrollPhysics()),
-                  scrollDirection: Axis.horizontal,
-                  controller: _scrollController,
-                  query: databaseRef,
-                  itemBuilder: (BuildContext context, DataSnapshot snapshot,
-                      Animation<double> animation, int index) {
-                    
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        width: 250,
-                        color: Colors.white60,
-                        child: ListTile(
-                          
-                          
-                            subtitle: Text(snapshot.child("title").value.toString()),
-                            title: Text(snapshot.child("price").value.toString() + "kr"),
-                            leading: Image.network(downloadURLExample(snapshot.child("aid").value.toString()) as String,
-                            height: 50,
-                            width: 50,)
-                            
-                            
-                            ),
-                        
-                      ),
-                    );
-                  },
-                ),
-              
-                      ),
+              },
             ),
-          IconButton(icon: Icon(Icons.arrow_forward), onPressed: (){
-                _scrollController.animateTo(
-                  _scrollController.offset + 150,
-                  duration: Duration(milliseconds: 500),
-                  curve: Curves.easeOut,
-                );
-            },),
-          
+          ),
+          Expanded(
+            child: Container(
+              height: 300,
+              child: FirebaseAnimatedList(
+                physics: ScrollPhysics(parent: ClampingScrollPhysics()),
+                scrollDirection: Axis.horizontal,
+                controller: _scrollController,
+                query: databaseRef,
+                itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                    Animation<double> animation, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 250,
+                      color: Colors.white60,
+                      child: ListTile(
+                          subtitle:
+                              Text(snapshot.child("title").value.toString()),
+                          title: Text(
+                              snapshot.child("price").value.toString() + "kr"),
+                          leading: Image.network(
+                              "https://firebasestorage.googleapis.com/v0/b/ungansatt123.appspot.com/o/0bec91f9-2264-4273-9e8f-6aeb2f53b5ac%2Fwp11093306-bmw-m5-2022-wallpapers.jpg?alt=media&token=59266e96-f193-4ace-ab9d-3aa129cf8f44")),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.arrow_forward),
+            onPressed: () {
+              _scrollController.animateTo(
+                _scrollController.offset + 150,
+                duration: Duration(milliseconds: 500),
+                curve: Curves.easeOut,
+              );
+            },
+          ),
         ],
       ),
     );
--- lib/pages/newJob/widgets/large_newjob.dart
+++ lib/pages/newJob/widgets/large_newjob.dart
@@ -25,7 +25,8 @@ class LargeNewJob extends StatelessWidget {
       body: Container(
         decoration: const BoxDecoration(
             image: DecorationImage(
-                image: AssetImage('assets/images/newjob_background.jpg'),
+                image: NetworkImage(
+                    'https://firebasestorage.googleapis.com/v0/b/ungansatt123.appspot.com/o/0bec91f9-2264-4273-9e8f-6aeb2f53b5ac%2Fwp11093306-bmw-m5-2022-wallpapers.jpg?alt=media&token=59266e96-f193-4ace-ab9d-3aa129cf8f44'),
                 fit: BoxFit.cover)),
         child: Column(
           children: [const Nav(), FillOut()],
@@ -37,7 +38,6 @@ class LargeNewJob extends StatelessWidget {
 
 class FillOut extends StatefulWidget {
   FillOut({super.key});
-
 
   @override
   State<FillOut> createState() => _FillOutState();
@@ -52,27 +52,27 @@ class _FillOutState extends State<FillOut> {
   FirebaseStorage _storage = FirebaseStorage.instance;
 
   late Uint8List selectedImageInBytes;
-  List<Uint8List> pickedImagesInBytes= [];
+  List<Uint8List> pickedImagesInBytes = [];
   String selectFile = '';
   int imageCounts = 0;
 
   _selectFile(bool imageFrom) async {
-  FilePickerResult? fileResult =
-      await FilePicker.platform.pickFiles(allowMultiple: true);
+    FilePickerResult? fileResult =
+        await FilePicker.platform.pickFiles(allowMultiple: true);
 
-  if (fileResult != null) {
-    selectFile = fileResult.files.first.name;
-    fileResult.files.forEach((element) {
-      setState(() {
-        pickedImagesInBytes.add(element.bytes as Uint8List);
-        selectedImageInBytes = fileResult.files.first.bytes!;
-        imageCounts += 1;
+    if (fileResult != null) {
+      selectFile = fileResult.files.first.name;
+      fileResult.files.forEach((element) {
+        setState(() {
+          pickedImagesInBytes.add(element.bytes as Uint8List);
+          selectedImageInBytes = fileResult.files.first.bytes!;
+          imageCounts += 1;
+        });
       });
-    });
+    }
+    print(selectFile);
+    print(imageCounts);
   }
-  print(selectFile);
-  print(imageCounts);
-}
 
   upload() async {
     String title = titleCon.text.trim();
@@ -80,34 +80,41 @@ class _FillOutState extends State<FillOut> {
     String address = addressCon.text.trim();
     String zipcode = zipcodeCon.text.trim();
     String price = priceCon.text.trim();
+    String imageUrl = '';
 
     var uuid = Uuid();
     var aid = uuid.v4();
 
-    DatabaseReference ref =
-        FirebaseDatabase.instance.ref("adventures");
+    final StorageRef = _storage.ref().child(aid).child('/' + selectFile);
+
+    final metadata = SettableMetadata(contentType: 'image/jpeg');
+
+    await StorageRef.putData(selectedImageInBytes, metadata);
+
+    try {
+      imageUrl =
+          await StorageRef.getDownloadURL().then((value) => value as String);
+    } catch (e) {
+      print("Error getting download URL: $e");
+    }
+
+    print(imageUrl);
+
+    DatabaseReference ref = FirebaseDatabase.instance.ref("adventures");
 
     String uid = FirebaseAuth.instance.currentUser!.uid;
 
-    ref.child(aid).set({
+    await ref.child(aid).set({
       "title": title,
       "descprition": descprition,
       "address": address,
       "zipcode": zipcode,
       "price": price,
       "uid": uid,
-      "aid": aid
+      "aid": aid,
+      "img1": imageUrl,
     });
-
-    final StorageRef = _storage.ref().child(aid).child('/' + selectFile);
-
-    final metadata =
-          SettableMetadata(contentType: 'image/jpeg');
-
-      StorageRef.putData(selectedImageInBytes, metadata);
-
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -183,20 +190,20 @@ class _FillOutState extends State<FillOut> {
                         padding: EdgeInsets.only(left: 50, bottom: 7, top: 10),
                         child: Text('Bilder'),
                       ),
-                    ),Expanded(
-                        
+                    ),
+                    Expanded(
                         child: selectFile.isEmpty
-                      ? SizedBox(
-                        height: 1,
-                      )
-                      : Padding(
-                        padding: const EdgeInsets.only(bottom: 20),
-                        child: SizedBox(height: 100,child: Image.memory(selectedImageInBytes)),
-                      )),
-                    
+                            ? SizedBox(
+                                height: 1,
+                              )
+                            : Padding(
+                                padding: const EdgeInsets.only(bottom: 20),
+                                child: SizedBox(
+                                    height: 100,
+                                    child: Image.memory(selectedImageInBytes)),
+                              )),
                   ],
                 ),
-                
                 SizedBox(
                   width: 350,
                   height: 40,
@@ -225,25 +232,26 @@ class _FillOutState extends State<FillOut> {
                   height: 7,
                 ),
                 SizedBox(
-                  width: 350,
-                  child: TextField(
-                    controller: addressCon,
-                    decoration: const InputDecoration(
-                      labelText: 'Gate',
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color.fromRGBO(102, 82, 143, 1.0), width: 2),
-                        borderRadius: BorderRadius.all(Radius.circular(2)),
+                    width: 350,
+                    child: TextField(
+                      controller: addressCon,
+                      decoration: const InputDecoration(
+                        labelText: 'Gate',
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Color.fromRGBO(102, 82, 143, 1.0),
+                              width: 2),
+                          borderRadius: BorderRadius.all(Radius.circular(2)),
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(2))),
                       ),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(2))),
-                    ),)),
-                  
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      child: const Padding(
-                        padding: EdgeInsets.only(left: 50, top: 30),
-                        child: Text('Hvor er dette?'),
+                    )),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: const Padding(
+                    padding: EdgeInsets.only(left: 50, top: 30),
+                    child: Text('Hvor er dette?'),
                   ),
                 ),
                 const SizedBox(
@@ -350,4 +358,3 @@ class Nav extends StatelessWidget {
     );
   }
 }
-
