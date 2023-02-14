acbe44765c1f7bd21ddb01d4dda5afe9217e2849
Bolle Benni <Bruker@IKT-TippiToppi.iktService.local>
2023-02-14 14:16:45 +0100

    jobslist update

--- lib/pages/home/widgets/job_list.dart
+++ lib/pages/home/widgets/job_list.dart
@@ -1,9 +1,14 @@
 import 'dart:async';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:webdesign/pages/job/job_view.dart';
 
 class JobList extends StatefulWidget {
   const JobList({super.key});
@@ -19,60 +24,97 @@ class _JobListState extends State<JobList> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            child: IconButton(
-              icon: Icon(Icons.arrow_back),
+      child: Container(
+        constraints: BoxConstraints(maxWidth: 1500),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  _scrollController.animateTo(
+                    _scrollController.offset - 150,
+                    duration: Duration(milliseconds: 500),
+                    curve: Curves.easeOut,
+                  );
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                height: 300,
+                child: FirebaseAnimatedList(
+                  physics: ScrollPhysics(parent: ClampingScrollPhysics()),
+                  scrollDirection: Axis.horizontal,
+                  controller: _scrollController,
+                  query: databaseRef,
+                  itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                      Animation<double> animation, int index) {
+                    return InkWell(
+                      onTap: (){
+                        String aid = snapshot.child('aid').value.toString();
+                        String uid = snapshot.child('uid').value.toString();
+                        String img1 = snapshot.child('img1').value.toString();
+                        String title = snapshot.child('title').value.toString();
+                        String descprition = snapshot.child('description').value.toString();
+                        String price = snapshot.child('price').value.toString();
+                        String address = snapshot.child('address').value.toString();
+                        String zipcode = snapshot.child('zipcode').value.toString();
+
+                        Get.to(() => JobView(
+                        aid: aid, 
+                        uid: uid, 
+                        img1: img1, 
+                        title: title, 
+                        descprition: descprition, 
+                        price: price, 
+                        address: address, 
+                        zipcode: zipcode));
+                      },
+                      child: Container(
+                          alignment: Alignment.topCenter,
+                          width: 330,
+                          child: ListTile(
+                              title: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white70,
+                                  borderRadius: BorderRadius.circular(5)
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.only(topRight: Radius.circular(5), topLeft: Radius.circular(5)),
+                                      child: Image.network(snapshot.child('img1').value.toString(), width: 330,
+                                      height: 250,
+                                      fit: BoxFit.cover,),
+                                    ),
+                                    Text(snapshot.child('title').value.toString(), style: GoogleFonts.tinos(fontSize: 20),) ,
+                                    Text(snapshot.child('price').value.toString() + ' kr', style: GoogleFonts.tinos(fontSize: 15),),
+                                  ],
+                                ),
+                              ),
+                            )
+                          ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.arrow_forward),
               onPressed: () {
                 _scrollController.animateTo(
-                  _scrollController.offset - 150,
+                  _scrollController.offset + 150,
                   duration: Duration(milliseconds: 500),
                   curve: Curves.easeOut,
                 );
               },
             ),
-          ),
-          Expanded(
-            child: Container(
-              height: 300,
-              child: FirebaseAnimatedList(
-                physics: ScrollPhysics(parent: ClampingScrollPhysics()),
-                scrollDirection: Axis.horizontal,
-                controller: _scrollController,
-                query: databaseRef,
-                itemBuilder: (BuildContext context, DataSnapshot snapshot,
-                    Animation<double> animation, int index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      width: 250,
-                      color: Colors.white60,
-                      child: ListTile(
-                          subtitle:
-                              Text(snapshot.child("title").value.toString()),
-                          title: Text(
-                              snapshot.child("price").value.toString() + "kr"),
-                          leading: Image.network(
-                              "https://firebasestorage.googleapis.com/v0/b/ungansatt123.appspot.com/o/0bec91f9-2264-4273-9e8f-6aeb2f53b5ac%2Fwp11093306-bmw-m5-2022-wallpapers.jpg?alt=media&token=59266e96-f193-4ace-ab9d-3aa129cf8f44")),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
-          IconButton(
-            icon: Icon(Icons.arrow_forward),
-            onPressed: () {
-              _scrollController.animateTo(
-                _scrollController.offset + 150,
-                duration: Duration(milliseconds: 500),
-                curve: Curves.easeOut,
-              );
-            },
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
