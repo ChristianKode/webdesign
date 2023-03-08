9566208786d1beb197427194e546e0fe13193266
HeavyBenni <benjamin.csaplar005@gmail.com>
2023-03-08 13:48:49 +0100

    homepage

--- lib/pages/home/widgets/home_job_list.dart
+++ lib/pages/home/widgets/home_job_list.dart
@@ -19,157 +19,165 @@ class _JobListState extends State<JobList> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        color: const Color.fromARGB(7, 0, 0, 0),
-        constraints: const BoxConstraints(maxWidth: 1500),
-        height: 500,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  width: 30,
-                ),
-                IconButton(
-                  icon: const Icon(Icons.arrow_back),
-                  onPressed: () {
-                    _scrollController.animateTo(
-                      _scrollController.offset - 350,
-                      duration: const Duration(milliseconds: 500),
-                      curve: Curves.easeOut,
-                    );
-                  },
-                ),
-                Expanded(
-                  child: SizedBox(
-                    height: 350,
-                    child: FirebaseAnimatedList(
-                      physics:
-                          const ScrollPhysics(parent: ClampingScrollPhysics()),
-                      scrollDirection: Axis.horizontal,
-                      controller: _scrollController,
-                      query: databaseRef,
-                      itemBuilder: (BuildContext context, DataSnapshot snapshot,
-                          Animation<double> animation, int index) {
-                        return InkWell(
-                            highlightColor: Colors.transparent,
-                            splashColor: Colors.transparent,
-                            hoverColor: Colors.transparent,
-                            onTap: () {
-                              String aid =
-                                  snapshot.child('aid').value.toString();
-                              String uid =
-                                  snapshot.child('uid').value.toString();
-                              String img1 =
-                                  snapshot.child('img1').value.toString();
-                              String title =
-                                  snapshot.child('title').value.toString();
-                              String descprition = snapshot
-                                  .child('descprition')
-                                  .value
-                                  .toString();
-                              String price =
-                                  snapshot.child('price').value.toString();
-                              String address =
-                                  snapshot.child('address').value.toString();
-                              String zipcode =
-                                  snapshot.child('zipcode').value.toString();
+    return Container(
+      color: const Color.fromARGB(7, 0, 0, 0),
+      width: MediaQuery.of(context).size.width,
+      child: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 1500),
+          height: 500,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    width: 30,
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back),
+                    onPressed: () {
+                      _scrollController.animateTo(
+                        _scrollController.offset - 350,
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.easeOut,
+                      );
+                    },
+                  ),
+                  Expanded(
+                    child: SizedBox(
+                      height: 350,
+                      child: FirebaseAnimatedList(
+                        physics: const ScrollPhysics(
+                            parent: ClampingScrollPhysics()),
+                        scrollDirection: Axis.horizontal,
+                        controller: _scrollController,
+                        query: databaseRef,
+                        itemBuilder: (BuildContext context,
+                            DataSnapshot snapshot,
+                            Animation<double> animation,
+                            int index) {
+                          return InkWell(
+                              highlightColor: Colors.transparent,
+                              splashColor: Colors.transparent,
+                              hoverColor: Colors.transparent,
+                              onTap: () {
+                                String aid =
+                                    snapshot.child('aid').value.toString();
+                                String uid =
+                                    snapshot.child('uid').value.toString();
+                                String img1 =
+                                    snapshot.child('img1').value.toString();
+                                String title =
+                                    snapshot.child('title').value.toString();
+                                String descprition = snapshot
+                                    .child('descprition')
+                                    .value
+                                    .toString();
+                                String price =
+                                    snapshot.child('price').value.toString();
+                                String address =
+                                    snapshot.child('address').value.toString();
+                                String zipcode =
+                                    snapshot.child('zipcode').value.toString();
 
-                              Get.to(() => JobView(
-                                  aid: aid,
-                                  uid: uid,
-                                  img1: img1,
-                                  title: title,
-                                  descprition: descprition,
-                                  price: price,
-                                  address: address,
-                                  zipcode: zipcode));
-                            },
-                            child: Padding(
-                              padding:
-                                  const EdgeInsets.only(right: 35, bottom: 10),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(5),
-                                  boxShadow: const [
-                                    BoxShadow(
-                                      color: Color.fromARGB(20, 0, 0, 0),
-                                      blurRadius: 2,
-                                      offset: Offset(0.5, 5), // Shadow position
-                                    ),
-                                  ],
-                                ),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Padding(
-                                      padding:
-                                          const EdgeInsets.only(bottom: 12),
-                                      child: ClipRRect(
-                                        borderRadius: const BorderRadius.only(
-                                            topRight: Radius.circular(5),
-                                            topLeft: Radius.circular(5)),
-                                        child: Image.network(
-                                          snapshot
-                                              .child('img1')
-                                              .value
-                                              .toString(),
-                                          width: 300,
-                                          height: 270,
-                                          fit: BoxFit.cover,
+                                Get.to(() => JobView(
+                                    aid: aid,
+                                    uid: uid,
+                                    img1: img1,
+                                    title: title,
+                                    descprition: descprition,
+                                    price: price,
+                                    address: address,
+                                    zipcode: zipcode));
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                    right: 35, bottom: 10),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(5),
+                                    boxShadow: const [
+                                      BoxShadow(
+                                        color: Color.fromARGB(20, 0, 0, 0),
+                                        blurRadius: 2,
+                                        offset:
+                                            Offset(0.5, 5), // Shadow position
+                                      ),
+                                    ],
+                                  ),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(bottom: 12),
+                                        child: ClipRRect(
+                                          borderRadius: const BorderRadius.only(
+                                              topRight: Radius.circular(5),
+                                              topLeft: Radius.circular(5)),
+                                          child: Image.network(
+                                            snapshot
+                                                .child('img1')
+                                                .value
+                                                .toString(),
+                                            width: 310,
+                                            height: 270,
+                                            fit: BoxFit.cover,
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                    Padding(
-                                      padding:
-                                          const EdgeInsets.only(left: 12.0),
-                                      child: Text(
-                                        snapshot
-                                            .child('title')
-                                            .value
-                                            .toString(),
-                                        style: GoogleFonts.tinos(
-                                            fontSize: 20,
-                                            fontWeight: FontWeight.w700),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 12.0),
+                                        child: Text(
+                                          snapshot
+                                              .child('title')
+                                              .value
+                                              .toString(),
+                                          style: GoogleFonts.tinos(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.w700),
+                                        ),
                                       ),
-                                    ),
-                                    Padding(
-                                      padding:
-                                          const EdgeInsets.only(left: 12.0),
-                                      child: Text(
-                                        '${snapshot.child('price').value} kr',
-                                        style: GoogleFonts.tinos(fontSize: 17),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 12.0),
+                                        child: Text(
+                                          '${snapshot.child('price').value} kr',
+                                          style:
+                                              GoogleFonts.tinos(fontSize: 17),
+                                        ),
                                       ),
-                                    ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            ));
-                      },
+                              ));
+                        },
+                      ),
                     ),
                   ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.arrow_forward),
-                  onPressed: () {
-                    _scrollController.animateTo(
-                      _scrollController.offset + 350,
-                      duration: const Duration(milliseconds: 500),
-                      curve: Curves.easeOut,
-                    );
-                  },
-                ),
-                const SizedBox(
-                  width: 30,
-                )
-              ],
-            ),
-          ],
+                  IconButton(
+                    icon: const Icon(Icons.arrow_forward),
+                    onPressed: () {
+                      _scrollController.animateTo(
+                        _scrollController.offset + 350,
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.easeOut,
+                      );
+                    },
+                  ),
+                  const SizedBox(
+                    width: 30,
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/home/widgets/home_owners.dart
+++ lib/pages/home/widgets/home_owners.dart
@@ -21,55 +21,76 @@ class SmallHomeOwnersWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 700,
+      height: 500,
       color: const Color.fromARGB(255, 67, 46, 140),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text.rich(TextSpan(
-              style: const TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
-              children: [
-                TextSpan(
-                    text: 'UngAnsatt-',
-                    style: GoogleFonts.tinos(
-                        fontWeight: FontWeight.bold, color: Colors.white)),
-              ])),
-          Text.rich(TextSpan(
-              style: const TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-              children: [
-                TextSpan(
-                    text: 'Jobberfaring for deg',
-                    style: GoogleFonts.tinos(
-                        fontWeight: FontWeight.bold, color: Colors.white)),
-              ])),
-          Padding(
-            padding: const EdgeInsets.only(top: 30),
-            child: Text('Visjonen vår er å få ungdom ut i arbeid',
-                style: GoogleFonts.tinos(fontSize: 30, color: Colors.white)),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 30),
-            child: Text(
-              'og skape dem erfaring for resten av livet',
-              style: GoogleFonts.tinos(fontSize: 30, color: Colors.white),
+      child: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text.rich(TextSpan(
+                style:
+                    const TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+                children: [
+                  TextSpan(
+                      text: 'UngAnsatt-',
+                      style: GoogleFonts.tinos(
+                          fontWeight: FontWeight.bold, color: Colors.white)),
+                ])),
+            Text.rich(TextSpan(
+                style:
+                    const TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+                children: [
+                  TextSpan(
+                      text: 'Jobberfaring for deg',
+                      style: GoogleFonts.tinos(
+                          fontWeight: FontWeight.bold, color: Colors.white)),
+                ])),
+            Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child: Text('Visjonen vår er å få ungdom ut i arbeid',
+                  style: GoogleFonts.tinos(fontSize: 30, color: Colors.white)),
             ),
-          ),
-          Row(
-            children: [
-              const Icon(
-                Icons.check_circle_outline,
-                color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30),
+              child: Text(
+                'og skape dem erfaring for resten av livet',
+                style: GoogleFonts.tinos(fontSize: 30, color: Colors.white),
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: Text(
-                  'Plukk etter preferansen din',
-                  style: GoogleFonts.tinos(color: Colors.white, fontSize: 20),
+            ),
+            Row(
+              children: [
+                const Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.white,
                 ),
-              )
-            ],
-          ),
-        ],
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text(
+                    'Plukk etter preferansen din',
+                    style: GoogleFonts.tinos(color: Colors.white, fontSize: 20),
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 60),
+              child: SizedBox(
+                  height: 35,
+                  width: 180,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: const Color.fromARGB(255, 44, 176, 110)),
+                      onPressed: () {},
+                      child: const Text(
+                        'Utforsk',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ))),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -142,7 +163,7 @@ class LargeHomeOwnersWidget extends StatelessWidget {
                       Padding(
                         padding: const EdgeInsets.only(left: 8.0),
                         child: Text(
-                          'Plukk etter preferansen din',
+                          'Meld deg på oppdrag som passer deg',
                           style: GoogleFonts.tinos(
                               color: Colors.white, fontSize: 20),
                         ),
@@ -160,7 +181,7 @@ class LargeHomeOwnersWidget extends StatelessWidget {
                         Padding(
                           padding: const EdgeInsets.only(left: 8.0),
                           child: Text(
-                            'Plukk etter preferansen din',
+                            'Få relevant arbeidserfaring',
                             style: GoogleFonts.tinos(
                                 color: Colors.white, fontSize: 20),
                           ),
